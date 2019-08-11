77cb544bb268f7ec00d168f4204f3d763dbba5da
Machanga <florenceshiru765@gmail.com>
2019-08-11 22:09:56 +0300

    add fields to the neighborhood class

--- loop/models.py
+++ loop/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Neighborhood(models.Model):
     neighborhood_name = models.CharField(max_length = 60)
-    neighborhood_location = models.CharField(max_length = 60)
+    neighborhood_location = models.ForeignKey('Location',on_delete = models.CASCADE,null = True)
     occupants = models.IntegerField()
+    admin = models.ForeignKey(User,on_delete = models.CASCADE)
 
