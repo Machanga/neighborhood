96acbda8e253902baf48ccb8a8a4a50f1adf40a2
Machanga <florenceshiru765@gmail.com>
2019-08-11 22:27:02 +0300

    create the business model class that will store all details about various businesses in a particular neighborhood

--- loop/models.py
+++ loop/models.py
@@ -44,3 +44,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Business(models.Model):
+    name = models.CharField(max_length = 60)
+    user = models.ForeignKey(User,on_delete = models.CASCADE)
+    neighborhood = models.ForeignKey(Neighborhood,on_delete = models.CASCADE)
+    email = models.EmailField(max_length = 60)
+    
+    def __str__(self):
+        return self.name
