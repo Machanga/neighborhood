48b9083e1c0a6c755faf53d83f721ade260a7a7f
Machanga <florenceshiru765@gmail.com>
2019-08-11 22:04:35 +0300

    add the sign up and login forms that a user will user to create an account and log into the application

--- loop/models.py
+++ loop/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Neighborhood(models.Model):
+    neighborhood_name = models.CharField(max_length = 60)
+    neighborhood_location = models.CharField(max_length = 60)
+    occupants = models.IntegerField()
+
