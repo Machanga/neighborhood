124b8a9a688920895daf05ad130e2aee459e4bb7
Machanga <florenceshiru765@gmail.com>
2019-08-11 22:48:07 +0300

    craete the business form that a user will fill in once they want to post their business in the neighborhood

--- loop/forms.py
+++ loop/forms.py
@@ -4,3 +4,8 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('name','id','neighborhood','email')
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ['user','neighborhood']
